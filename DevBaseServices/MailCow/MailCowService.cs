8122167a4bdd9126771410988a4500c42ded3cc6
EinfachEinAlex <mitteodersoyt@gmail.com>
2021-08-06 13:45:32 +0200

    + Basic Service Request Structure

---
+++ DevBaseServices/MailCow/MailCowService.cs
@@ -0,0 +1,38 @@
+﻿using DevBase.Web.RequestData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DevBaseServices.MailCow
+{
+    //Untested
+    class MailCowService
+    {
+        private Uri _serverUri;
+        private string _apiKey;
+
+        public MailCowService(Uri serverUri, string apiKey)
+        {
+            this._serverUri = serverUri;
+            this._apiKey = apiKey;
+        }
+
+        public string SendApiRequest(IServiceData serviceData)
+        {
+            RequestData requestData = new RequestData(
+                new Uri(_serverUri, serviceData.EndpointDirectory(), false), 
+                RequestMethod.POST, 
+                ContentType.JSON, 
+                serviceData.RequestString());
+
+            requestData.Header.Add("X-API-Key", this._apiKey);
+
+            DevBase.Web.Request request = new DevBase.Web.Request(requestData);
+            
+            return Encoding.ASCII.GetString(request.GetResponse().Content);
+        }
+    }
+}
