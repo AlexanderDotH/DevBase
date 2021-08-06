a61ac4cc39ea53ad39e806c8a1999a890eecc0dd
EinfachEinAlex <mitteodersoyt@gmail.com>
2021-08-06 13:45:32 +0200

    + Basic Service Request Structure

---
+++ DevBaseServices/MailCow/Requests/CreateMailBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DevBaseServices.MailCow.Requests
+{
+    class CreateMailBox : IServiceData
+    {
+        public string EndpointDirectory() => "/api/v1/add/mailbox";
+
+        private CreateMailBoxObject _createMailBoxObject;
+
+        public CreateMailBox(CreateMailBoxObject createMailBoxObject)
+        {
+            this._createMailBoxObject = createMailBoxObject;
+        }
+
+        public string RequestString()
+        {
+            return JsonSerializer.Serialize(_createMailBoxObject);
+        }
+    }
+}
---
+++ DevBaseServices/MailCow/Requests/CreateMailBoxObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBaseServices.MailCow.Requests
+{
+    class CreateMailBoxObject
+    {
+        public bool active;
+        public string domain;
+        public string local_part;
+        public string name;
+        public string password;
+        public string password2;
+        public int quota;
+        public bool force_pw_update;
+        public bool tls_enforce_in;
+        public bool tls_enforce_out;
+    }
+}
