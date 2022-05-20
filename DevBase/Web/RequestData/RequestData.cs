ecfc3ddd048959bcfd6e0d8dc4d0804a53d44c87
EinfachEinAlex <mitteodersoyt@gmail.com>
2022-05-20 15:40:43 +0200

    Added RandomUserAgents

--- DevBase/Web/RequestData/RequestData.cs
+++ DevBase/Web/RequestData/RequestData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using DevBase.Generic;
 
 namespace DevBase.Web.RequestData
 {
@@ -65,6 +67,36 @@ namespace DevBase.Web.RequestData
                Encoding.Default.GetBytes(string.Empty), 
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
                ) { }
+
+        public string GetRandomUseragent()
+        {
+            GenericList<string> userAgents = new GenericList<string>();
+            userAgents.Add("Mozilla/5.0 (iPad; CPU OS 7_2_1 like Mac OS X; en-US) AppleWebKit/531.43.4 (KHTML, like Gecko) Version/3.0.5 Mobile/8B112 Safari/6531.43.4");
+            userAgents.Add("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_8_7) AppleWebKit/5351 (KHTML, like Gecko) Chrome/37.0.881.0 Mobile Safari/5351");
+            userAgents.Add("Mozilla/5.0 (Windows CE) AppleWebKit/5330 (KHTML, like Gecko) Chrome/37.0.896.0 Mobile Safari/5330");
+            userAgents.Add("Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 4.0; Trident/4.0)");
+            userAgents.Add("Opera/8.57 (X11; Linux x86_64; en-US) Presto/2.10.234 Version/11.00");
+            userAgents.Add("Mozilla/5.0 (X11; Linux i686) AppleWebKit/5331 (KHTML, like Gecko) Chrome/40.0.842.0 Mobile Safari/5331");
+            userAgents.Add("Mozilla/5.0 (iPad; CPU OS 8_1_2 like Mac OS X; sl-SI) AppleWebKit/533.27.6 (KHTML, like Gecko) Version/3.0.5 Mobile/8B114 Safari/6533.27.6");
+            userAgents.Add("Opera/9.63 (X11; Linux i686; sl-SI) Presto/2.12.215 Version/11.00");
+            userAgents.Add("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_8_9) AppleWebKit/5340 (KHTML, like Gecko) Chrome/40.0.816.0 Mobile Safari/5340");
+            userAgents.Add("Mozilla/5.0 (iPhone; CPU iPhone OS 8_2_1 like Mac OS X; en-US) AppleWebKit/535.45.2 (KHTML, like Gecko) Version/3.0.5 Mobile/8B111 Safari/6535.45.2");
+            userAgents.Add("Mozilla/5.0 (Windows NT 5.2) AppleWebKit/5362 (KHTML, like Gecko) Chrome/39.0.880.0 Mobile Safari/5362");
+            userAgents.Add("Mozilla/5.0 (iPad; CPU OS 7_2_1 like Mac OS X; en-US) AppleWebKit/532.5.1 (KHTML, like Gecko) Version/3.0.5 Mobile/8B119 Safari/6532.5.1");
+            userAgents.Add("Opera/8.23 (Windows NT 5.01; en-US) Presto/2.12.266 Version/12.00");
+            userAgents.Add("Mozilla/5.0 (compatible; MSIE 9.0; Windows 98; Trident/5.1)");
+            userAgents.Add("Opera/8.87 (Windows NT 6.1; sl-SI) Presto/2.11.204 Version/10.00");
+            userAgents.Add("Mozilla/5.0 (Windows; U; Windows NT 6.1) AppleWebKit/534.9.5 (KHTML, like Gecko) Version/4.0.1 Safari/534.9.5");
+            userAgents.Add("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 5.2; Trident/4.1)");
+            userAgents.Add("Mozilla/5.0 (Windows NT 5.01; en-US; rv:1.9.1.20) Gecko/20140618 Firefox/35.0");
+            userAgents.Add("Opera/9.64 (Windows 98; Win 9x 4.90; en-US) Presto/2.10.219 Version/12.00");
+            userAgents.Add("Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X; en-US) AppleWebKit/535.7.5 (KHTML, like Gecko) Version/4.0.5 Mobile/8B113 Safari/6535.7.5");
+            userAgents.Add("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/5330 (KHTML, like Gecko) Chrome/37.0.885.0 Mobile Safari/5330");
+            userAgents.Add("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_8_2 rv:3.0; sl-SI) AppleWebKit/534.17.2 (KHTML, like Gecko) Version/5.1 Safari/534.17.2");
+            userAgents.Add("Mozilla/5.0 (Macintosh; PPC Mac OS X 10_6_0 rv:3.0) Gecko/20120102 Firefox/37.0");
+
+            return userAgents.Get(new Random().Next(0, userAgents.Length));
+        }
 
         public string ConvertFromContentType(string existingContentType, ContentType[] requestType)
         {
