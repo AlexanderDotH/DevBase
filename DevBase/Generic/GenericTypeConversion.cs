db63e78daa315c236754eb3b56b5c0124f689d2f
AlexH <mitteodersoyt@gmail.com>
2022-07-28 14:56:46 +0200

    Generic type conversion

---
+++ DevBase/Generic/GenericTypeConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBase.Generic
+{
+    public class GenericTypeConversion<F, T>
+    {
+        public GenericList<T> MergeToList(GenericList<F> inputList, Action<F, GenericList<T>> action)
+        {
+            GenericList<T> convertToList = new GenericList<T>();
+
+            for (int i = 0; i < inputList.Length; i++)
+            {
+                F input = inputList[i];
+                action(input, convertToList);
+            }
+
+            return convertToList;
+        }
+
+        public GenericList<T> MergeToList(List<F> inputList, Action<F, GenericList<T>> action)
+        {
+            GenericList<T> convertToList = new GenericList<T>();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                F input = inputList[i];
+                action(input, convertToList);
+            }
+
+            return convertToList;
+        }
+    }
+}
---
+++ DevBaseEnvironment/Program.cs
@@ -0,0 +1,53 @@
+﻿using DevBase.Generic;
+
+namespace DevBaseEnvironment
+{
+    public class Track
+    {
+        public string Name { get; set; }
+    }
+
+    public class Results
+    {
+        public List<TrackList> Tracks { get; set; }
+
+        public class TrackList
+        {
+            public Track Track;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Results results = new Results();
+            
+            Track t1 = new Track();
+            t1.Name = "SUMMER OF MY LIFE";
+
+            Track t2 = new Track();
+            t2.Name = "Summer";
+
+            results.Tracks = new List<Results.TrackList>();
+
+            Results.TrackList results1 = new Results.TrackList();
+            results1.Track = t1;
+
+            Results.TrackList results2 = new Results.TrackList();
+            results2.Track = t2;
+
+            results.Tracks.Add(results1);
+            results.Tracks.Add(results2);
+
+            List<Track> merge = new GenericTypeConversion<Results.TrackList, Track>().MergeToList(results.Tracks, (input,tracks) => tracks.Add(input.Track));
+
+            foreach (Track t in merge)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+        }
+    }
+}
