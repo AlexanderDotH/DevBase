8d640a03fd1f410bbf9470c0aac50ef259143ca0
AlexH <mitteodersoyt@gmail.com>
2023-02-07 22:52:48 +0100

    Removed scaling

--- DevBase.Avalonia/Scaling/BindingContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevBase.Avalonia.Scaling;
-
-/// <summary>
-/// Represents a context that can be scaled by the <see cref="ScalingManager"/>.
-/// </summary>
-public class BindingContext : HashSet<ScalableObject>
-{
-}
--- DevBase.Avalonia/Scaling/IViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevBase.Avalonia.Scaling;
-
-/// <summary>
-/// Indicates that a class acts as a view model for a corresponding <see cref="Window"/>.
-/// </summary>
-public interface IViewModel
-{
-    public bool IsResizing { get; set; }
-}
