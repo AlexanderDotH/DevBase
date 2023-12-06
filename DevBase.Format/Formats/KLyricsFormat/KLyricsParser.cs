d4549366f03277c8eb21a765704ad4736f010098
AlexanderDotH <mitteodersoyt@gmail.com>
2023-12-06 14:05:35 +0100

    feat(parsing): Implemented KLyrics parser
    Integrated KLyrics parsing into the existing parser structure by adding relevant regular expressions and match processes. This includes timestamps, data elements, and identifying the end of KLyrics. The commit also covers conversion of these elements to required formats.
    
    BREAKING CHANGE: Not build ready yet.

--- DevBase.Format/Formats/KLyricsFormat/KLyricsParser.cs
+++ DevBase.Format/Formats/KLyricsFormat/KLyricsParser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using DevBase.Format.Structure;
 using DevBase.Generics;
 using DevBase.IO;
@@ -8,6 +9,17 @@ namespace DevBase.Format.Formats.KLyricsFormat;
 
 public class KLyricsParser : IFileFormat<AList<RichLyrics>>
 {
+    private readonly Regex _regexTimeStamp;
+    private readonly Regex _regexElements;
+    private readonly Regex _regexLastWord;
+
+    public KLyricsParser()
+    {
+        this._regexTimeStamp = new Regex(RegexHolder.REGEX_KLYRICS_TIMESTAMPS);
+        this._regexElements = new Regex(RegexHolder.REGEX_KLRICS_DATA);
+        this._regexLastWord = new Regex(RegexHolder.REGEX_KLYRICS_END);
+    }
+    
     public AList<RichLyrics> FormatFromFile(string filePath)
     {
         AFileObject file = AFile.ReadFile(filePath);
@@ -16,23 +28,65 @@ public class KLyricsParser : IFileFormat<AList<RichLyrics>>
 
     public AList<RichLyrics> FormatFromString(string lyricString)
     {
+        List<(MatchCollection, MatchCollection, MatchCollection)> lines = GetLines(lyricString);
         
     }
 
-    private int GetFirstLine(string lyricString)
+    private RichLyrics ProcessLine(
+        MatchCollection timestamps, 
+        MatchCollection wordElements,
+        MatchCollection lastWordElements)
     {
-        AList<string> lines = new AString(lyricString).AsList();
+        int lineStartTimeStamp = Convert.ToInt32(timestamps[0].Groups[2].Value);
+        int lineEndTimeStamp = Convert.ToInt32(timestamps[0].Groups[4].Value);
 
-        for (int i = 0; i < lines.Length; i++)
+    }
+
+    private List<RichLyricsElement> ProcessElements(int lineStartTimeStamp, MatchCollection wordElements)
+    {
+        List<RichLyricsElement> elements = new List<RichLyricsElement>();
+
+        for (int i = 0; i < wordElements.Count; i++)
         {
-            string c = lines.Get(i);
-
-            (TimeSpan StartTime, TimeSpan EndTime) timeSpans = ParseTimeStamp(lyricString);
+            Match match = wordElements[i];
             
+            int elementStartTimeStamp = Convert.ToInt32(match.Groups[5].Value);
+            
+            if (wordElements.Count )
         }
     }
 
-    
+    private List<(MatchCollection, MatchCollection, MatchCollection)> GetLines(string lyricString)
+    {
+        AList<string> lines = new AString(lyricString).AsList();
+
+        List<(MatchCollection, MatchCollection, MatchCollection)> matchLines =
+            new List<(MatchCollection, MatchCollection, MatchCollection)>();
+        
+        for (int i = 0; i < lines.Length; i++)
+            matchLines.Add(GetLine(lines.Get(i)));
+
+        return matchLines;
+    }
+
+    public (MatchCollection TimeStamps, MatchCollection WordElements, MatchCollection LastWordElement) GetLine(
+        string current)
+    {
+        MatchCollection timestamps = null;
+        MatchCollection wordElements = null;
+        MatchCollection lastWordElement = null;
+        
+        if (this._regexTimeStamp.IsMatch(current))
+            timestamps = this._regexTimeStamp.Matches(current);
+
+        if (this._regexElements.IsMatch(current))
+            wordElements = this._regexElements.Matches(current);
+
+        if (this._regexLastWord.IsMatch(current))
+            lastWordElement = this._regexLastWord.Matches(current);
+
+        return (timestamps, wordElements, lastWordElement);
+    }
     
     public (TimeSpan StartTime, TimeSpan EndTime) ParseTimeStamp(string line)
     {
--- DevBase.Format/Structure/RegexHolder.cs
+++ DevBase.Format/Structure/RegexHolder.cs
@@ -9,5 +9,9 @@
         public const string REGEX_ENV = "(\\w*)\\=\"(\\w*)";
         public const string REGEX_SRT_TIMESTAMPS = "(\\d...........)\\D....(............)";
         public const string REGEX_ELRC_DATA = "(\\[)([0-9]*)([:])([0-9]*)([:])(\\d+\\.\\d+|\\d+)(\\])(\\s-\\s)(\\[)([0-9]*)([:])([0-9]*)([:])(\\d+\\.\\d+|\\d+)(\\])\\s(.*$)";
+        
+        public const string REGEX_KLYRICS_TIMESTAMPS = "(\\[)([0-9]*)(\\,)([0-9]*)(\\])";
+        public const string REGEX_KLRICS_DATA = "((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*)(\\()([0-9]*)(\\,)([0-9]*)(\\)))";
+        public const string REGEX_KLYRICS_END = "(((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*$)))";
     }
 }
