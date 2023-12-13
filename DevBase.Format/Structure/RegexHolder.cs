6c3fe4fb83cc2b22e86e4d82d27651598b79bf08
Alex <alexander.heuschkel@gmail.com>
2023-12-14 00:10:12 +0100

    chore(codebase): Added some regexes

--- DevBase.Format/Structure/RegexHolder.cs
+++ DevBase.Format/Structure/RegexHolder.cs
@@ -8,8 +8,12 @@
         public const string REGEX_ENV = "(\\w*)\\=\"(\\w*)";
         public const string REGEX_SRT_TIMESTAMPS = "([0-9:,]*)(\\W(-->)\\W)([0-9:,]*)";
         public const string REGEX_ELRC_DATA = "(\\[)([0-9]*)([:])([0-9]*)([:])(\\d+\\.\\d+|\\d+)(\\])(\\s-\\s)(\\[)([0-9]*)([:])([0-9]*)([:])(\\d+\\.\\d+|\\d+)(\\])\\s(.*$)";
-        public const string REGEX_KLYRICS_TIMESTAMPS = "(\\[)([0-9]*)(\\,)([0-9]*)(\\])";
-        public const string REGEX_KLRICS_DATA = "((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*)(\\()([0-9]*)(\\,)([0-9]*)(\\))(\\s))";
-        public const string REGEX_KLYRICS_END = "(((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*$)))";
+        
+        // public const string REGEX_KLYRICS_TIMESTAMPS = "(\\[)([0-9]*)(\\,)([0-9]*)(\\])";
+        // public const string REGEX_KLRICS_DATA = "((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*)(\\()([0-9]*)(\\,)([0-9]*)(\\))(\\s))";
+        // public const string REGEX_KLYRICS_END = "(((\\()([0-9])(\\,)([0-9]*)(\\))([\\D]*$)))";
+
+        public const string REGEX_KLYRICS_WORD = "(\\()([0-9]*)(\\,)([0-9]*)(\\))([^\\(\\)\\[\\]\\n]*)";
+        public const string REGEX_KLYRICS_TIMESTAMPS = "(\\\\[)([0-9]*)(\\\\,)([0-9]*)(\\\\])";
     }
 }
