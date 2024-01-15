af15c3e18b45a8c0eca7064995e1bf714df4a408
AlexanderDotH <mitteodersoyt@gmail.com>
2024-01-15 12:12:58 +0100

    chore(codebase): Added user agent builder

--- DevBase.Requests/Preparation/Header/UserAgent/UserAgentHeaderBuilder.cs
+++ DevBase.Requests/Preparation/Header/UserAgent/UserAgentHeaderBuilder.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Buffers;
+using System.Reflection;
+using System.Text;
+using DevBase.Requests.Extensions;
 
 namespace DevBase.Requests.Preparation.Header.UserAgent;
 
@@ -7,6 +10,7 @@ public class UserAgentHeaderBuilder
     private StringBuilder _preGeneratedUserAgent;
 
     private string _productName;
+    private string _productVersion;
     
     public UserAgentHeaderBuilder()
     {
@@ -21,11 +25,38 @@ public class UserAgentHeaderBuilder
 
     public UserAgentHeaderBuilder AddProductVersion(string productVersion)
     {
-        
+        this._productVersion = productVersion;
+        return this;
     }
 
     public UserAgentHeaderBuilder Build()
     {
+        Assembly assembly = typeof(DevBase.Requests.Request).Assembly;
+        AssemblyName assemblyName = assembly.GetName();
+        
+        ReadOnlySpan<char> productName = this._productName;
+        ReadOnlySpan<char> productVersion = this._productVersion;
+        
+        if (productName.IsEmpty)
+            productName = assemblyName.Name;
+
+        if (productVersion.IsEmpty)
+            productVersion = assemblyName.Version!.ToString();
+
+        this._preGeneratedUserAgent.Append(productName);
+        this._preGeneratedUserAgent.Append('/');
+        this._preGeneratedUserAgent.Append(productVersion);
+        
         return this;
+    }
+
+    public ReadOnlySpan<char> UserAgent
+    {
+        get
+        {
+            char[] userAgent = Array.Empty<char>();
+            this._preGeneratedUserAgent.ToSpan(ref userAgent);
+            return userAgent;
+        }
     }
 }
---
+++ DevBase.Test/DevBaseRequests/Builder/UserAgentBuilderTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using DevBase.Requests.Preparation.Header.UserAgent;
+
+namespace DevBase.Test.DevBaseRequests.Builder;
+
+public class UserAgentBuilderTest
+{
+    [Test]
+    public void BuildUserAgentTest()
+    {
+        int count = 1_000_000;
+        
+        Stopwatch sw = new Stopwatch();
+        
+        sw.Start();
+        
+        UserAgentHeaderBuilder builder = new UserAgentHeaderBuilder();
+
+        for (int i = 0; i < count; i++)
+            builder = new UserAgentHeaderBuilder().Build();
+
+        ReadOnlySpan<char> agent = builder.UserAgent;
+        
+        sw.Stop();
+        
+        Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms or {sw.ElapsedTicks}ts to calculate the user-agent {count}times");
+        Console.WriteLine($"User-Agent: {agent}");
+    }
+}
