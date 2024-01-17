bfc2c40d278613d2a485f75e8d5ebd973475840c
Alex <alexander.heuschkel@gmail.com>
2024-01-17 16:59:11 +0100

    chore(codebase): Added bogus user agent generator for firefox

--- DevBase.Requests/Preparation/Header/UserAgent/Bogus/Generator/BogusFirefoxUserAgentGenerator.cs
+++ DevBase.Requests/Preparation/Header/UserAgent/Bogus/Generator/BogusFirefoxUserAgentGenerator.cs
@@ -1,22 +1,77 @@
 ﻿using System.Text;
+using DevBase.Requests.Extensions;
+using DevBase.Requests.Utils;
 
 namespace DevBase.Requests.Preparation.Header.UserAgent.Bogus.Generator;
 
 public class BogusFirefoxUserAgentGenerator : IBogusUserAgentGenerator
 {
-    private StringBuilder _generatedUserAgentPart;
-    
-    public BogusFirefoxUserAgentGenerator()
+    private ReadOnlySpan<char> BogusFirefoxUserAgent()
     {
-        this._generatedUserAgentPart = new StringBuilder();
-        
-        GenerateUserAgent();
-    }
+        StringBuilder firefoxUserAgent = new StringBuilder();
 
-    private void GenerateUserAgent()
-    {
+        ReadOnlySpan<char> product = BogusUtils.RandomProductName();
+        ReadOnlySpan<char> productVersion = BogusUtils.RandomProductVersion();
+
+        PlatformID platformId = BogusUtils.RandomPlatformId();
+
+        ReadOnlySpan<char> osPlatform = BogusUtils.RandomOperatingSystem(platformId);
+
+        ReadOnlySpan<char> firefoxProduct = "Firefox";
+        ReadOnlySpan<char> firefoxVersion = RandomFirefoxVersion();
+
+        ReadOnlySpan<char> geckoEngine = "Gecko";
+        ReadOnlySpan<char> geckoTrail = "20100101";
+
+        // Mozilla/5.0
+        firefoxUserAgent.Append(product);
+        firefoxUserAgent.Append('/');
+        firefoxUserAgent.Append(productVersion);
+        firefoxUserAgent.Append(' ');
+
+        // (Windows NT 6.1; Win64; rv:42.0)
+        firefoxUserAgent.Append('(');
+        firefoxUserAgent.Append(osPlatform);
+        firefoxUserAgent.Append(' ');
+        firefoxUserAgent.Append('r');
+        firefoxUserAgent.Append('v');
+        firefoxUserAgent.Append(':');
+        firefoxUserAgent.Append(firefoxVersion);
+        firefoxUserAgent.Append(')');
+        firefoxUserAgent.Append(' ');
         
+        // Gecko/20100101
+        firefoxUserAgent.Append(geckoEngine);
+        firefoxUserAgent.Append('/');
+        firefoxUserAgent.Append(geckoTrail);
+        firefoxUserAgent.Append(' ');
+
+        // Firefox/42.0
+        firefoxUserAgent.Append(firefoxProduct);
+        firefoxUserAgent.Append('/');
+        firefoxUserAgent.Append(firefoxVersion);
+
+        char[] userAgent = Array.Empty<char>();
+        firefoxUserAgent.ToSpan(ref userAgent);
+
+        return userAgent;
     }
     
-    public ReadOnlySpan<char> UserAgentPart { get; }
+    private ReadOnlySpan<char> RandomFirefoxVersion()
+    {
+        StringBuilder firefoxVersion = new StringBuilder();
+        
+        ReadOnlySpan<char> randomVersion = BogusUtils.Random.Next(40, 121).ToString();
+
+        firefoxVersion.Append(randomVersion);
+        firefoxVersion.Append('.');
+        firefoxVersion.Append('0');
+
+        char[] version = Array.Empty<char>();
+        firefoxVersion.ToSpan(ref version);
+
+        return version;
+    }
+    
+    public ReadOnlySpan<char> UserAgentPart => BogusFirefoxUserAgent();
 }
--- DevBase.Requests/Utils/BogusUtils.cs
+++ DevBase.Requests/Utils/BogusUtils.cs
@@ -1,24 +1,175 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
+using DevBase.Requests.Extensions;
 
 namespace DevBase.Requests.Utils;
 
 public class BogusUtils
 {
-    private static (string identifier, bool hasSub) _plattforms;
+    private static (char[] Value, PlatformID PlatformId)[] _desktopOperatingSystems;
+    private static (char[] Value, PlatformID PlatformId)[] _desktopOperatingSystemsArchitecture;
 
+    private static char[][] _product;
+    private static char[][] _productVersion;
+
+    private static Random _random;
+    
     static BogusUtils()
     {
-        _plattforms = new[]
+        _desktopOperatingSystems = new (char[] Value, PlatformID PlatformId)[]
         {
-            "Windows NT",
-            "Linux; Android",
-            "X11; Linux x86_64"
+            ("Windows NT".ToCharArray(), PlatformID.Win32NT), // Windows also has a version after like "6.1"
+            ("Macintosh".ToCharArray(), PlatformID.MacOSX),
+            ("X11".ToCharArray(), PlatformID.Unix)
         };
+        
+        _desktopOperatingSystemsArchitecture = new (char[] Value, PlatformID PlatformId)[]
+        {
+            ("Win64".ToCharArray(), PlatformID.Win32NT),
+            ("Win32".ToCharArray(), PlatformID.Win32NT),
+            ("Intel Mac OS X".ToCharArray(), PlatformID.MacOSX), // Mac also has a version agter it "14_2_1"
+            ("Linux x86_64".ToCharArray(), PlatformID.Unix),
+            ("Linux i686".ToCharArray(), PlatformID.Unix)
+        };
+
+        _product = new[]
+        {
+            "Mozilla".ToCharArray()
+        };
+        
+        _productVersion = new[]
+        {
+            "5.0".ToCharArray()
+        };
+
+        _random = new Random();
+    }
+
+    public static PlatformID RandomPlatformId()
+    {
+        int osType = _random.Next(1, 3) * 2;
+        return (PlatformID)osType;
+    }
+
+    public static ReadOnlySpan<char> RandomOS(PlatformID platformId)
+    {
+        (char[] Value, PlatformID PlatformId)[] available = _desktopOperatingSystems
+            .Where(c => c.PlatformId == platformId)
+            .ToArray();
+
+        return available[_random.Next(0, available.Length)].Value;
     }
     
-    public static string RandomOperatingSystemPlattform()
+    public static ReadOnlySpan<char> RandomArchitecture(PlatformID platformId)
     {
+        (char[] Value, PlatformID PlatformId)[] available = _desktopOperatingSystemsArchitecture
+            .Where(c => c.PlatformId == platformId)
+            .ToArray();
+
+        return available[_random.Next(0, available.Length)].Value;
+    }
+
+    public static ReadOnlySpan<char> RandomProductName()
+    {
+        return _product[_random.Next(_product.Length)];
+    }
+    
+    public static ReadOnlySpan<char> RandomProductVersion()
+    {
+        return _productVersion[_random.Next(_productVersion.Length)];
+    }
+    
+    public static ReadOnlySpan<char> RandomNTVersion()
+    {
+        StringBuilder ntBuilder = new StringBuilder();
+
+        ReadOnlySpan<char> major = _random.Next(1, 10).ToString();
+        ReadOnlySpan<char> minor = _random.Next(1, 9).ToString();
+
+        ntBuilder.Append(major);
+        ntBuilder.Append('.');
+        ntBuilder.Append(minor);
+
+        char[] ntVersion = Array.Empty<char>();
+        ntBuilder.ToSpan(ref ntVersion);
+
+        return ntVersion;
+    }
+
+    public static ReadOnlySpan<char> RandomIOSVersion()
+    {
+        StringBuilder iosBuilder = new StringBuilder();
+
+        ReadOnlySpan<char> major = _random.Next(1, 17).ToString();
+        ReadOnlySpan<char> minor = _random.Next(1, 9).ToString();
+        ReadOnlySpan<char> build = _random.Next(1, 9).ToString();
+
+        iosBuilder.Append(major);
+        iosBuilder.Append('_');
+        iosBuilder.Append(minor);
+        iosBuilder.Append('_');
+        iosBuilder.Append(build);
+
+        char[] iosVersion = Array.Empty<char>();
+        iosBuilder.ToSpan(ref iosVersion);
+
+        return iosVersion;
+    }
+
+    public static ReadOnlySpan<char> RandomOperatingSystem(PlatformID platformId)
+    {
+        StringBuilder osStringBuilder = new StringBuilder();
         
+        ReadOnlySpan<char> operatingSystem = RandomOS(platformId);
+        ReadOnlySpan<char> architecture = RandomArchitecture(platformId);
+
+        osStringBuilder.Append(operatingSystem);
+        
+        switch (platformId)
+        {
+            // Windows NT 6.1; Win64;
+            case PlatformID.Win32NT:
+            {
+                osStringBuilder.Append(' ');
+                osStringBuilder.Append(RandomNTVersion());
+                osStringBuilder.Append(';');
+                osStringBuilder.Append(' ');
+                osStringBuilder.Append(architecture);
+                osStringBuilder.Append(';');
+
+                break;
+            }
+            // Macintosh; Intel Mac OS X 10_9_2;
+            case PlatformID.MacOSX:
+            {
+                osStringBuilder.Append(';');
+                osStringBuilder.Append(' ');
+                osStringBuilder.Append(architecture);
+                osStringBuilder.Append(' ');
+                osStringBuilder.Append(RandomIOSVersion());
+                osStringBuilder.Append(';');
+                break;
+            }
+            // X11; Linux x86_64
+            case PlatformID.Unix:
+            {
+                osStringBuilder.Append(';');
+                osStringBuilder.Append(' ');
+                osStringBuilder.Append(architecture);
+                osStringBuilder.Append(';');
+                break;
+            }
+        }
+        
+        char[] os = Array.Empty<char>();
+        osStringBuilder.ToSpan(ref os);
+
+        return os;
+    }
+
+    public static Random Random
+    {
+        get => _random;
     }
 }
