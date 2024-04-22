748560c6d47c92717b0c86a23c7ed66769bdcec9
Alex <alexander.heuschkel@gmail.com>
2024-04-23 01:56:55 +0200

    chore(codebase): Added RunWithLast function

--- DevBase.Test/Test/PenetrationTest.cs
+++ DevBase.Test/Test/PenetrationTest.cs
@@ -4,6 +4,8 @@ namespace DevBase.Test.Test;
 
 public class PenetrationTest
 {
+    protected PenetrationTest() {}
+    
     public static Stopwatch Run(Action runAction, int count = 1_000_000)
     {
         Stopwatch stopwatch = new Stopwatch();
@@ -15,6 +17,22 @@ public class PenetrationTest
         
         stopwatch.Stop();
 
+        return stopwatch;
+    }
+    
+    public static Stopwatch RunWithLast<T>(Func<T> runAction, out T lastActionOutput, int count = 1_000_000)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        
+        stopwatch.Start();
+        
+        // For gods sake please don't specify count = 0 or some bs :)
+        for (int i = 0; i < count - 1; i++)
+            lastActionOutput = runAction.Invoke();
+
+        lastActionOutput = runAction.Invoke();
+        
+        stopwatch.Stop();
         return stopwatch;
     }
 }
